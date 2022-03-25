95401073db10442dc2ca85b2d370531c866c4ed4
Rakib-cloud <ri1641715@gmail.com>
2022-03-26 03:31:47 +0600

    random wise food select

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Cart.css';
 
 
 const Cart = (props) => {
     const {cart}=props;
-    //console.log(cart);
-   //console.log(cart[0].name);
-//    const {name}=cart[0].name;
-//    console.log(name);
-// let name;
-// for(const food of cart){
-//     name=food.name;
     
-// }
+    
+let index=[];
+//console.log(cart.id);
+cart.map(food=>{
+    index.push(food.id);
+})
+console.log(Math.floor(Math.random() * 4))
+
+
+function randomwise(cart){
+    let randname='';
+    console.log(cart)
+    cart.map(food=>{
+        
+        if(cart.length==4){
+            if(index[Math.floor(Math.random() * 4)]===food.id){
+                randname=food.name;
+                document.getElementById('rand').innerText=randname;
+            }
+            
+        }
+       
+    })
+    
+}
 
 
     return (
       
-        <div className='cart'>
+        
+        <div>
+             <div id='c' className='cart'>
             <h3>Food order Summary</h3>
             <p>food length:{cart.length}</p>
             <h5>order food list:</h5>
             <div>{cart.map(food =>
-    <h2>{food.name}</h2>
+    <h2 key={food.id}>{food.name}</h2>
 )}</div>
+<button onClick={()=>{randomwise(cart)}}>Get one food</button>
+           
+           <h2>Your selected food item :</h2>
+           <p id='rand'></p>
             
+        </div>
+        <button >Choose again</button>
         </div>
     );
 };
