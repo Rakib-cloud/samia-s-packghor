f11b55967a0d337c5e217f37e5bc90cf172add9f
Rakib-cloud <ri1641715@gmail.com>
2022-03-26 13:38:59 +0600

    some change

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './Question.css';
 
 const Question = () => {
     return (
-        <div>
+        <div className='ques'>
             <div className='ques1'>
                 <h2>How React Works?</h2>
                 <p>If there are numerous state changes, React does not commit them in order. React, on the other hand, walks through its virtual DOM, makes a list of the modifications that need to be applied to the actual DOM, and then performs everything in one step. React does batch updates, to put it another way.!</p>
             </div>
             <div className='ques2'>
                 <h2>How Usestate works</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ex quae totam perferendis possimus velit quos consequuntur magnam ea labore?</p>
+                <p>useState is a Hook that enables state variables being used in functional components. This function will take the initial state and generates a variable with the current state value (not necessarily the starting state) and another function to update it.</p>
             </div>
            
         </div>
